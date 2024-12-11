b38a6a752544825fb1a1135e2df36bd02a8bec07
Your Name <khalidsaifullaha3@gmail.com>
2024-12-11 22:30:55 +0600

    jobApply form

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const JobDetails = () => {
   const job = useLoaderData();
-  const { title, company, deadline } = job;
+  const { _id, title, company, deadline } = job;
   return (
     <div className="m-10">
       <h2>Job Details for {title}</h2>
       <p>Apply for: {company}</p>
       <p>Deadline: {deadline}</p>
-      <button className="btn btn-primary">Apply Now</button>
+      <Link to={`/jobApply/${_id}`}>
+        <button className="btn btn-primary">Apply Now</button>
+      </Link>
     </div>
   );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,6 +6,7 @@ import SignIn from "../pages/Signin/SignIn";
 import Home from "../pages/Home/Home";
 import JobDetails from "../pages/JobDetails/JobDetails";
 import PrivateRoute from "./PrivateRoute";
+import JobApply from "../pages/JobApply/JobApply";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,16 @@ const router = createBrowserRouter([
         ),
         loader: ({ params }) =>
           fetch(`http://localhost:3000/jobs/${params.id}`),
+      },
+      {
+        path: "/jobApply/:id",
+        element: (
+          <PrivateRoute>
+            <JobApply></JobApply>
+          </PrivateRoute>
+        ),
+        // loader: ({ params }) =>
+        //   fetch(`http://localhost:3000/jobs/${params.id}`),
       },
       {
         path: "/register",
