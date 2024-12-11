9f37985e56e85ff53eedd8b704aa7c89ada69d97
Your Name <khalidsaifullaha3@gmail.com>
2024-12-11 23:14:43 +0600

    job application

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
   const { id } = useParams();
-  console.log(id);
+  const { user } = useAuth();
+  console.log(id, user);
 
   const submitJobApplication = (e) => {
     e.preventDefault();
@@ -12,7 +15,35 @@ const JobApply = () => {
     const github = form.github.value;
     const resume = form.resume.value;
 
-    console.log(linkedIn, github, resume);
+    // console.log(linkedIn, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+
+    fetch("http://localhost:3000/job-applications", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(jobApplication),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
   };
   return (
     <div className="card bg-base-100 w-full   shadow-2xl">
